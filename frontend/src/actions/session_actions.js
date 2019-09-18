c84eafb1ed89333de8ed7abea44911b32b46c686
Chin Wee Nie <chinweenie@gmail.com>
2019-09-18 23:32:35 +0800

    Pull form master

--- frontend/src/actions/session_actions.js
+++ frontend/src/actions/session_actions.js
@@ -1,5 +1,5 @@
+// import * as SessionApiUtil from '../util/session_api_util';
 import * as SessionApiUtil from '../util/session_api_util';
-
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
 export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
@@ -34,8 +34,9 @@ export const login = (user) => dispatch => (
   ))
 );
 
-export const logout = () => dispatch => (
-  SessionApiUtil.logout().then(() => (
+export const logout = () => dispatch => {
+  debugger
+  return SessionApiUtil.logout().then(() => (
     dispatch(logoutCurrentUser())
   ))
-);
+};
