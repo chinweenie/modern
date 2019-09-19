e5f314f879d5d9e6680b8111257b48268dc84b29
Chin Wee Nie <chinweenie@gmail.com>
2019-09-20 07:01:46 +0800

    Add story errors reducer

---
+++ frontend/src/reducers/story_errors_reducer.js
@@ -0,0 +1,20 @@
+import { RECEIVE_STORIES_ERRORS, RECEIVE_STORY, RECEIVE_ALL_STORIES } from "../actions/stories_actions";
+RECEIVE_ALL_STORIES
+
+const storyErrorsReducer = (state = [], action) => {
+    switch (action.type) {
+        case RECEIVE_STORIES_ERRORS:
+            return action.errors;
+            
+        case RECEIVE_STORY:
+            return [];
+            
+        case RECEIVE_ALL_STORIES:
+            return [];
+    
+        default:
+            return [];
+    }
+}
+
+export default storyErrorsReducer;
