2c3818b20645723e918577615a1305fb69b4b83f
Roger Kiew <yuichiu416@gmail.com>
2019-09-21 12:01:14 -0500

    move the stories from profile reducer to story reducer

--- frontend/src/reducers/stories_reducer.js
+++ frontend/src/reducers/stories_reducer.js
@@ -1,4 +1,4 @@
-import { RECEIVE_ALL_STORIES, RECEIVE_STORY, REMOVE_STORY } from "../actions/stories_actions";
+import { RECEIVE_ALL_STORIES, RECEIVE_STORY, REMOVE_STORY, RECEIVE_STORIES } from "../actions/stories_actions";
 
 const storiesReducer = (state = {}, action) => {
     Object.freeze(state);
@@ -15,6 +15,9 @@ const storiesReducer = (state = {}, action) => {
             return Object.assign({}, state, {
                 [action.story._id]: action.story
             });
+        case RECEIVE_STORIES:
+            const stories = action.stories.map(o => { return { title: o.title, body: o.body } });
+            return Object.assign({}, { stories: stories });
         case REMOVE_STORY:
             newObject = Object.assign({}, state);
             delete newObject[action.storyId];
--- frontend/src/util/profile_util.js
+++ frontend/src/util/profile_util.js
@@ -1,9 +1,5 @@
 import axios from 'axios';
 
-export const fetchStoriesOfOneUser = user => {
-    return axios.get(`/@${user.username}/stories/${user.id}`);
-};
-
 export const updateStory = storyData => {
     return axios.patch(`/api/stories/${storyData.id}`, storyData);
 };
